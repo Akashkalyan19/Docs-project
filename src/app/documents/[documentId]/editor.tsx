496cd320516d2e948c977a52e6c164b78c9338af
AkashKalyan19 <Akshaykalyan7@gmail.com>
2025-01-05 12:05:26 +0530

    adding ruler component

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -3,7 +3,7 @@ import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Image from "@tiptap/extension-image";
 import { Color } from "@tiptap/extension-color";
-// import ImageResize from "tiptap-extension-image-resize";
+import { Ruler } from "./ruler";
 import Link from "@tiptap/extension-link";
 import FontFamily from "@tiptap/extension-font-family";
 import ImageResize from "tiptap-extension-resize-image";
@@ -22,6 +22,7 @@ import { useEditorStore } from "@/store/use-editor-store";
 import { FontSizeExtension } from "@/extension/font-size";
 import { LineHeightExtension } from "@/extension/line-height";
 export const Editor = () => {
+  immediatelyRender: false;
   const { setEditor } = useEditorStore();
   const editor = useEditor({
     onCreate({ editor }) {
@@ -101,6 +102,7 @@ export const Editor = () => {
   });
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
--- src/app/documents/[documentId]/layout.tsx
+++ src/app/documents/[documentId]/layout.tsx
@@ -2,11 +2,6 @@ interface DocumentsLayoutProps {
   children: React.ReactNode;
 }
 const DocumentsLayout = ({ children }: DocumentsLayoutProps) => {
-  return (
-    <div className="flex flex-col gap-y-4">
-      <nav className="w-full bg-red-500">Document navbar</nav>
-      {children}
-    </div>
-  );
+  return <div className="flex flex-col gap-y-4">{children}</div>;
 };
 export default DocumentsLayout;
