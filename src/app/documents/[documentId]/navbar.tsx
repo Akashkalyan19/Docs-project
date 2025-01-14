02cd53094a3f29bf6c4279d4f58337184e0279fe
AkashKalyan19 <Akshaykalyan7@gmail.com>
2025-01-14 16:05:41 +0530

    adding user info

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -34,6 +34,7 @@ import {
   UnderlineIcon,
   Undo2Icon,
 } from "lucide-react";
+import { Avatars } from "./avatars";
 import { useEditorStore } from "@/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 export const Navbar = () => {
@@ -247,6 +248,7 @@ export const Navbar = () => {
         </div>
       </div>
       <div className="flex gap-3 items-center pl-6">
+        <Avatars />
         <OrganizationSwitcher
           afterCreateOrganizationUrl="/"
           afterLeaveOrganizationUrl="/"
